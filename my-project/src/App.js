d7aceebf1e6d6ad0e77be66054b387f6cbddfae5
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-09-02 10:49:33 +0530

    commit

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
 export const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
---
+++ my-project/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Home = () => {
+  return (
+    <div>
+        <p>Hi , my name is</p>
+        <h1>K.Bogeshwararao</h1>
+        <h2></h2>
+    </div>
+  )
+}
+export default Home
+
