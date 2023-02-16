9aca74e873268561acc2658fcc178a4ebc4d4675
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-02-16 13:04:37 +0630

    complted

---
+++ myport/src/Components/Navbar.js
@@ -0,0 +1,44 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter ,
+  Routes,
+  Route,
+  Link } from "react-router-dom";
+import Home from "../Screens/Home";
+import Skills from "../Screens/Skills"
+import Project from "../Screens/Project"
+import Blog from "../Screens/Blog"
+
+function CollapsibleExample() {
+  return (
+    <BrowserRouter>
+   
+    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark"  fixed="top"  >
+      <Container>
+        <Navbar.Brand href="#home">KBR</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/"  className='nav-item'>Home</Nav.Link>
+            <Nav.Link as={Link} to="/Skills"  className='nav-item'>Skills</Nav.Link>
+            <Nav.Link as={Link} to="/Project"  className='nav-item'>Project</Nav.Link>
+            <Nav.Link as={Link} to="/Blog"  className='nav-item'>Blog</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div>
+    <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/SKills' element={<Skills />}/>
+        <Route path='/Project' element={<Project />}/>
+        <Route path='/Blog' element={<Blog />}/>
+        </Routes>
+    
+   </div>
+    </BrowserRouter>
+  );
+}
+
+export default CollapsibleExample;
---
+++ myport/src/Components/ProjectData.jsx
@@ -0,0 +1,26 @@
+import Climate from "../Images/Weather0.jpg";
+import BMI from "../Images/bmi.jpg";
+import ACM from "../Images/acmprojectimg.jpg"
+const ProjectData = [
+  {
+    title: "Climate Check",
+    description:
+      "I recently developed a weather website using a variety of technologies, including an API for retrieving weather data, Node.js for server-side scripting, Express.js for routing, and HTML for the front-end user interface. The website allows users to input their location and view current weather conditions.",
+    repo: "https://github.com/Bogeshwararao/Weather-Report-",
+    imgsrc: Climate,
+  },
+  {
+    title: "B.M.I",
+    description:"I have developed a BMI calculator using Express and Node.js, which can be accessed through an HTML interface. The BMI calculator takes in a person's weight and height as input and calculates their BMI, which is a measure of body fat based on their weight in relation to their height.",
+    repo: "https://github.com/Bogeshwararao/B.M.I",
+    imgsrc:BMI,
+  },
+  {
+    title: "Official ACM website",
+    description:"I'm happy to describe the website I have developed for a community using MERN stack services.The website you have built is a feature-rich, full-stack web application that is built using MongoDB,Express,React and Node.js.The MERN stack services have been used to create a enjoyable user experience for the community.",
+    repo: "https://github.com/Bogeshwararao/B.M.I",
+    imgsrc:ACM,
+  }
+];
+
+export default ProjectData;
