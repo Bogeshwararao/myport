3fd31ed51991e25dabafe7764b2411255abecd8a
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-02-16 15:11:19 +0630

    commit

--- myport/src/Components/ProjectData.jsx
+++ myport/src/Components/ProjectData.jsx
@@ -1,6 +1,9 @@
 import Climate from "../Images/Weather0.jpg";
 import BMI from "../Images/bmi.jpg";
 import ACM from "../Images/acmprojectimg.jpg"
+import Portfolio from "../Images/portfolioproject.jpg"
+
+
 const ProjectData = [
   {
     title: "Climate Check",
@@ -10,17 +13,23 @@ const ProjectData = [
     imgsrc: Climate,
   },
   {
-    title: "B.M.I",
-    description:"I have developed a BMI calculator using Express and Node.js, which can be accessed through an HTML interface. The BMI calculator takes in a person's weight and height as input and calculates their BMI, which is a measure of body fat based on their weight in relation to their height.",
-    repo: "https://github.com/Bogeshwararao/B.M.I",
-    imgsrc:BMI,
+    title: "Portfolio",
+    description:"I are excited to announce the recent launch of a website I developed for showcase My work.The website was built using the React.js and Bootstrap frameworks to provide a responsive.Overall, we believe that the combination of React.js and Bootstrap has allowed us to create a fast,and user-friendly website",
+    repo: "https://github.com/Bogeshwararao/bogesh",
+    imgsrc: Portfolio,
   },
   {
     title: "Official ACM website",
     description:"I'm happy to describe the website I have developed for a community using MERN stack services.The website you have built is a feature-rich, full-stack web application that is built using MongoDB,Express,React and Node.js.The MERN stack services have been used to create a enjoyable user experience for the community.",
     repo: "https://github.com/Bogeshwararao/B.M.I",
     imgsrc:ACM,
-  }
+  },
+  {
+    title: "B.M.I",
+    description:"I have developed a BMI calculator using Express and Node.js, which can be accessed through an HTML interface. The BMI calculator takes in a person's weight and height as input and calculates their BMI, which is a measure of body fat based on their weight in relation to their height.",
+    repo: "https://github.com/Bogeshwararao/B.M.I",
+    imgsrc:BMI,
+  },
 ];
 
 export default ProjectData;
