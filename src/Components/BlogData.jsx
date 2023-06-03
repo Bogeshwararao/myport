f0c2092cbc35b64d6d210b27d79fd9ec6ba34b31
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-04 05:19:38 +0530

    commit

--- src/Components/BlogData.jsx
+++ src/Components/BlogData.jsx
@@ -1,16 +1,15 @@
-// // eslint-disable-next-line
-// import git from "../Images/git.jpeg"
+// eslint-disable-next-line
+import git from "../Images/git.jpeg"
 
-// const BlogData = [
-//   {
-//     title: "GIT COMMANDS",
-//     description:
-//       "Hi there! Basically, people are very much less aware of git and Github. I'm sure that won't happen after going through this entire article.",
-//     published: "May 29, 2023",
-//     read: "4",
-//     imgsrc: git,
-//     link: "https://bogeshwararao.hashnode.dev/top-git-commands",
-//   }
-// ];
+const BlogData = [
+  {
+    title: "GIT COMMANDS",
+    description:
+      "Hi there! Basically, people are very much less aware of git and Github. I'm sure that won't happen after going through this entire article.",
+    published: "May 29, 2023",
+    imgsrc: git,
+    link: "https://bogeshwararao.hashnode.dev/top-git-commands",
+  }
+];
 
-// export default BlogData;
+export default BlogData;
